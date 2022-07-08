373f94fb22d867914054f6331df1f485e0f965a4
Javierfg86 <Javierfg86@outlook.es>
2022-07-08 19:07:05 +0200

    Add supertest and fux user-requiredforSuitcases

--- src/middleware/user-required.ts
+++ src/middleware/user-required.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { ExtRequest } from '../interfaces/token.js';
 import { Suitcase } from '../models/suitcase.model.js';
+// import { User } from '../models/user.model.js';
 
 export const userRequiredForSuitcase = async (
     req: Request,
@@ -9,11 +10,30 @@ export const userRequiredForSuitcase = async (
 ) => {
     const userID = (req as unknown as ExtRequest).tokenPayload.id;
     const findSuitcase = await Suitcase.findById(req.params.id);
-    if (findSuitcase?.owner === userID) {
+    if (userID === String(findSuitcase?.owner)) {
         next();
     } else {
         const error = new Error();
         error.name = 'UserAuthorizationError';
         next(error);
+        console.log(userID, findSuitcase?.owner);
     }
 };
+//Add Requirer user for delete user!!
+// export const userRequiredForUser = async (
+//     req: Request,
+//     res: Response,
+//     next: NextFunction
+// ) => {
+//     // el usuario tiene acceso al recurso (e.g. Tarea)
+
+//     const userID = (req as unknown as ExtRequest).tokenPayload.id;
+//     const findUser = await User.findById(req.params.id);
+//     if (findUser?.responsible === userID) {
+//         next();
+//     } else {
+//         const error = new Error();
+//         error.name = 'UserAuthorizationError';
+//         next(error);
+//     }
+// };
