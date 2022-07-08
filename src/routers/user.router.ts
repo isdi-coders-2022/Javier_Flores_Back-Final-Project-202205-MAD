dd8152ae8d20b0ce49b6018b4e043222cc79cfdd
Javierfg86 <Javierfg86@outlook.es>
2022-07-09 01:17:26 +0200

    Add middleware user-require for users

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { User } from '../models/user.model.js';
 import { UserController } from '../controllers/user.controller.js';
 import { loginRequired } from '../middleware/login-required.js';
+import { userRequiredForUser } from '../middleware/user-required.js';
 
 export const userController = new UserController(User);
 export const userRouter = Router();
@@ -10,5 +11,17 @@ userRouter.get('/', userController.getAllController);
 userRouter.get('/:id', userController.getController);
 userRouter.post('/', userController.postController); // registro
 userRouter.post('/login', userController.loginController);
-userRouter.patch('/:id', loginRequired, userController.patchController);
-userRouter.delete('/:id', loginRequired, userController.deleteController);
+userRouter.patch(
+    '/:id',
+
+    loginRequired,
+    userRequiredForUser,
+    userController.patchController
+);
+userRouter.delete(
+    '/:id',
+
+    loginRequired,
+    userRequiredForUser,
+    userController.deleteController
+);
